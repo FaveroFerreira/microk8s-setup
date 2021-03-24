d22727883214ea7062f235f9ade1dfa6a3e1c62b
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2021-03-24 15:34:45 -0300

    feat(kubectl install): dryrun

--- kubectl-install.sh
+++ kubectl-install.sh
@@ -1,1 +1,26 @@
-echo "teste"
+#!/bin/sh
+set -e
+
+do_install() {
+    echo "# Executando instalação do kubectl"
+}
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+dry_run() {
+    if command_exists kubectl; then
+        cat >&2 <<-'EOF'
+			Aviso: "kubectl" parece já estar instalado neste sistema.
+
+			Abortando a instalação.
+		EOF
+
+        exit 1
+    fi
+
+    
+}
+
+dry_run
